cab34767101426c925f29370414427b499e9599d
Tarik02 <Taras.Fomin@gmail.com>
2022-09-15 14:06:51 +0300

    set: return object after changing it

--- set.ts
+++ set.ts
@@ -1,6 +1,6 @@
 // WARNING: This is not a drop in replacement solution and
 // it might not work for some edge cases. Test your code!
-export default (obj: any, path: string | string[], value: any) => {
+export default (obj: any, path: string | string[], value: any): any => {
     // Regex explained: https://regexr.com/58j0k
     const pathArray = (
         Array.isArray(path) ? path : path.match(/([^[.\]])+/g)
@@ -28,4 +28,6 @@ export default (obj: any, path: string | string[], value: any) => {
 
         return acc[ key ];
     }, obj);
+
+    return obj;
 };
